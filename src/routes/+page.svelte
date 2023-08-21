05c64984a980cff917d87776ab51c49df1d868f0
omgaldamez <127152662+omgaldamez@users.noreply.github.com>
2023-08-21 02:25:14 -0600

    end

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,5 +11,17 @@
   
 <Portfolio />
 
+<div class="endOmia"></div>
+
 <style>
+.endOmia {
+    position: relative;
+    width: 100vw;
+    margin: 0;
+    background-image: url("./endOmia.png");
+    height: 100vh;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center calc(50% - 24px); /* Translate background image 24px up */
+}
 </style>
